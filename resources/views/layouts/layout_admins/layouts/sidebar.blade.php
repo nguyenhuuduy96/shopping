db8d25da0d6335a77d6437bf3fd4c84834f4f13e
nguyenhuuduy0796 <ngguyenhuuduy0796@gmail.com>
2020-10-22 11:17:31 +0700

    update

--- resources/views/layouts/layout_admins/layouts/sidebar.blade.php
+++ resources/views/layouts/layout_admins/layouts/sidebar.blade.php
@@ -115,7 +115,7 @@
             </ul>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
+          <a href="{{route('admin.setting')}}" class="nav-link">
               <i class="fas fa-cogs"></i>
               <p>Setting web</p>
             </a>
